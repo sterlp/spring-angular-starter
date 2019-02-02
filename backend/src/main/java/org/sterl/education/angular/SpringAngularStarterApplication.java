072c30d79b7e0a6f720f892ccfb664bf7c7427f4
Paul Sterl <paul@sterl.org>
2019-02-02 17:21:38 +0100

    first commit

---
+++ backend/src/main/java/org/sterl/education/angular/SpringAngularStarterApplication.java
@@ -0,0 +1,14 @@
+package org.sterl.education.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularStarterApplication.class, args);
+	}
+
+}
+
