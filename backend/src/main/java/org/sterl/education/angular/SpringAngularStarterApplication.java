9c6a54fc18e1a5f70bdb5b36394c6a39c03cfeba
Paul Sterl <paul.sterl@outlook.de>
2019-02-09 16:39:57 +0100

    added JAR solution to the code

--- backend/src/main/java/org/sterl/education/angular/SpringAngularStarterApplication.java
+++ backend/src/main/java/org/sterl/education/angular/SpringAngularStarterApplication.java
@@ -2,13 +2,23 @@ package org.sterl.education.angular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringAngularStarterApplication {
+public class SpringAngularStarterApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAngularStarterApplication.class, args);
 	}
-
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		/**
+		 * We can get the resources directly from the JAR file.
+		 * As so the front end JAR cannot be optional
+		 */
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/frontend/0.1.0-SNAPSHOT/");
+    }
 }
-
