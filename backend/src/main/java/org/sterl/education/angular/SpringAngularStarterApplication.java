6c133fe20b194a0db94829699ba0653324d65f22
Paul Sterl <paul.sterl@outlook.de>
2019-02-09 16:47:32 +0100

    disbaled the copy of the static resources

--- backend/src/main/java/org/sterl/education/angular/SpringAngularStarterApplication.java
+++ backend/src/main/java/org/sterl/education/angular/SpringAngularStarterApplication.java
@@ -3,6 +3,7 @@ package org.sterl.education.angular;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -12,13 +13,21 @@ public class SpringAngularStarterApplication implements WebMvcConfigurer {
 		SpringApplication.run(SpringAngularStarterApplication.class, args);
 	}
 	
+	/**
+	 * We can get the resources directly from the JAR file.
+	 * As so the front end JAR cannot be optional
+	 */
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		/**
-		 * We can get the resources directly from the JAR file.
-		 * As so the front end JAR cannot be optional
-		 */
 		registry.addResourceHandler("/**")
 				.addResourceLocations("classpath:/META-INF/resources/webjars/frontend/0.1.0-SNAPSHOT/");
     }
+	/**
+	 * This is needed in case we have no index.html in the static folder.
+	 * In the JAR solution we don't even have the static folder ...
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/").setViewName("forward:/index.html");
+	}
 }
